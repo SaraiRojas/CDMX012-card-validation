d0849be517fd668381d74d37014076bb125636de
Sarai Rojas <sara.r.sandoval@gmail.com>
2022-01-21 13:06:59 -0600

    Commit all changes to push to GitHub

--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
 import validator from './validator.js';
 
-console.log(validator);
+
+//Select input[type="text"]
+const cardNum = document.querySelector('#cardNum');
+const validate = document.querySelector('#validate');
+
+//Get the card number 
+let nums = "";
+
+cardNum.addEventListener('change', keepNum);
+
+
+function keepNum(e) {
+    nums = e.target.value.replace(/\s+/g, ''); /*Delate empty spaces*/ /*typeof num = string*/
+}
+
+//Gets numbers as typing
+/*
+
+cardNum.addEventListener('keyup', masking);
+
+function masking (e) {
+    cardNum.value = validator.maskify(e.target.value);
+
+}; */
+
+//Shows if the card number is valid or not 
+validate.addEventListener('click', showAnswer);
+
+function showAnswer( ) {
+    const resultBox = document.querySelector('#result');
+    const resultMessage =document.querySelector('#resultMessage');
+    let maskNums = validator.maskify(nums);
+    if(/^\d+$/.test(nums) && nums.length >= 13 && nums.length <= 18 && nums != ""){
+        let isValidResult = validator.isValid(nums);
+        
+        if(isValidResult){
+            resultBox.style.borderColor = 'var(--valid-color)';
+            resultBox.style.color = 'var(--valid-color)';
+            resultMessage.textContent = 'Valid card number';
+            cardNum.value = maskNums;
+            resultBox.style.display = 'block';
+            
+        }else{
+            resultBox.style.borderColor = 'var(--invalid-color)';
+            resultBox.style.color = 'var(--invalid-color)';
+            resultMessage.textContent = 'Invalid card number';
+            cardNum.value = maskNums;
+            resultBox.style.display = 'block';
+        }
+    }else{
+            resultBox.style.borderColor = 'var(--warning-color)';
+            resultBox.style.color = 'var(--warning-color)';
+            resultMessage.textContent = 'Please enter a card number';
+            cardNum.value = '';
+            resultBox.style.display = 'block';
+    }
+
+
+}
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,44 @@
 const validator = {
-  // ...
+  
+  isValid: (cardNum) => {
+
+    let inversedCardNum = [...cardNum].reverse();
+    let cardDoubleNums = [...cardNum].reverse();/*An array is created to storage doubled values*/
+    const reducer = (a, b) => Number(a) + Number(b); /* Function used in reduce method */
+
+    for (let i=1; i < cardNum.length; i+=2){
+        let doubleNum = inversedCardNum[i]*2; /* Data type of doubleNum is number */
+        if ( doubleNum > 9){
+            let oneDigit = doubleNum -9; 
+            cardDoubleNums.splice(i, 1, oneDigit); /*adds an element in a specific position to an Array*/
+        }else{
+            cardDoubleNums.splice(i, 1, doubleNum);
+        }
+    }
+
+    let sumNums = cardDoubleNums.reduce(reducer);
+
+    if (sumNums%10 == 0){
+        return true
+    }else{
+        return false
+    }
+  },
+
+  maskify: (cardNum) => {
+    
+    let cardNums = [...cardNum]; /*Creates an array out of cardNum*/
+    let maskNums = cardNums.map(function (num, i){
+        if (i <= cardNums.length-5){
+            return '#'
+        }else{
+            return num
+        }
+    });
+
+    return maskNums.join('') /*Returns an array as a string*/
+  }
+
 };
 
 export default validator;
